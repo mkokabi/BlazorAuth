02492ba8eaf5d269f61d2a63cbfe6e3039b0c3a7
Mohsen KOKABI <Mohsen.KOKABI@bambora.com>
2020-12-17 16:43:28 +1100

    Add project files.

---
+++ Client/Pages/DynamicForm.cs
@@ -0,0 +1,46 @@
+﻿using Hosted.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosted.Client.Pages
+{
+    public partial class DynamicForm
+    {
+        Form form;
+        internal static readonly Dictionary<string, string> ElementValues = new Dictionary<string, string>();
+
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+        [Inject]
+        IHttpClientFactory HttpClientFactory { get; set; }
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        internal string serverResponse;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var client = HttpClientFactory.CreateClient("PublicServerAPI");
+            var st = await client.GetStringAsync("Form");
+            form = JsonConvert.DeserializeObject<Form>(st, settings: new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+        }
+
+        private async Task Submit()
+        {
+            // await Http.PostJsonAsync<string>("Form", ElementValues); // wouldn't allow getting response
+            var strElementValues = JsonConvert.SerializeObject(ElementValues);
+            var response = await Http.PostAsync("Form", new StringContent(strElementValues, encoding: Encoding.UTF8, mediaType: "application/json"));
+            serverResponse = response.IsSuccessStatusCode ?
+                await response.Content.ReadAsStringAsync() :
+                response.StatusCode.ToString();
+        }
+    }
+}
---
+++ Server/Controllers/FormController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hosted.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hosted.Server.Controllers
+{
+    [Route("[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        [HttpGet]
+        public Form Get()
+        {
+            return new Form 
+            { 
+                Elements = new List<Element> 
+                {
+                    new TextInput
+                    {
+                        Name = "txtFName",  Label = "First Name", PlaceHolder="Enter your first name"
+                    },
+                    new TextInput
+                    {
+                        Name = "txtLName",  Label = "Last Name", PlaceHolder="Enter your last name",
+                        Value="Kokabi"
+                    },
+                    new RadioButton 
+                    { 
+                        Name = "radGender", Label = "Gender", 
+                        Options = new Dictionary<string, string> { { "M", "Male" }, {"F", "Female"} }                        
+                    }
+                }
+            };
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string Submit([FromBody] Dictionary<string, string> formValues)
+        {
+            return $"Hello {formValues["txtFName"]} {formValues["txtLName"]}";
+        }
+    }
+}
